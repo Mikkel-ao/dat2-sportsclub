b9e521b06e0f139a52ebafbab6d53ada06df867c
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-02-26 12:14:14 +0100

    Added dto package + class

--- src/main/java/dto/MemberAndSportsDTO.java
+++ src/main/java/dto/MemberAndSportsDTO.java
@@ -1,4 +1,22 @@
 package dto;
 
 public class MemberAndSportsDTO {
+    private String member;
+    private String sports;
+
+    public String getMember() {
+        return member;
+    }
+
+    public void setMember(String member) {
+        this.member = member;
+    }
+
+    public String getSports() {
+        return sports;
+    }
+
+    public void setSports(String sports) {
+        this.sports = sports;
+    }
 }
